e2609f63b3ab88e3c47da27e2836ce17b0122f28
Pratik Shinde <Pratikshinde1497>
2020-08-22 11:26:04 +0530

    hello world

---
+++ src/main/java/com/shinde/telusko/hibernate/App.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.shinde.telusko.hibernate;
+
+/**
+ *
+ * @author pratik
+ */
+public class App {
+    
+    public static void main(String[] args) {
+        System.out.println("com.shinde.telusko.hibernate.App.main()");
+        System.out.println("Hello world!");
+
+    }
+}
---
+++ src/main/java/com/shinde/telusko/hibernate/FoodItems.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.shinde.telusko.hibernate;
+
+/**
+ *
+ * @author pratik
+ */
+public class FoodItems {
+    private int fid;
+    private String fname;
+    private String details;
+    private int price;
+    private int quantity;
+
+    public int getFid() {
+        return fid;
+    }
+
+    public void setFid(int fid) {
+        this.fid = fid;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "FoodItems{" + "fid=" + fid + ", fname=" + fname + ", details=" + details + ", price=" + price + ", quantity=" + quantity + '}';
+    }
+    
+    
+}
